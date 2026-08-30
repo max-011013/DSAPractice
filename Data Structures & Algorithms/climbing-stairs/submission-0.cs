95024d47cfc19a0fd66e09597b41fa3ef1a49ffa
max-011013 <67928654+max-011013@users.noreply.github.com>
2026-08-30 16:22:25 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int ClimbStairs(int n) {     
+        if(n <= 1) return 1;
+        
+        int one = 1;  
+        int two = 1;  
+
+        for(int i = 0; i < n-1; i++){
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+
+        return one;
+    }
+}
